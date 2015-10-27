d9108662499a2d0b54fcd20f31400feb71ba0ecc
Igor_Seliverstov <Igor_Seliverstov@epam.com>
2015-10-27 16:52:18 +0300

    integrator

--- src/main/java/net/gvvinblade/study/hibernate/extension/MyIntegrator.java
+++ src/main/java/net/gvvinblade/study/hibernate/extension/MyIntegrator.java
@@ -1,7 +1,8 @@
 package net.gvvinblade.study.hibernate.extension;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.Collection;
-import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -9,9 +10,10 @@ import org.hibernate.boot.Metadata;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.hibernate.event.service.spi.EventListenerRegistry;
 import org.hibernate.event.spi.EventType;
-import org.hibernate.event.spi.SaveOrUpdateEventListener;
 import org.hibernate.integrator.spi.Integrator;
 import org.hibernate.service.spi.SessionFactoryServiceRegistry;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,17 +24,37 @@ import org.springframework.stereotype.Service;
 @Service
 public class MyIntegrator implements Integrator {
 
-    @Autowired(required = false)
-    private List<SaveOrUpdateEventListener> saveOrUpdateEventListeners;
+    @Autowired
+    private ListableBeanFactory beanFactory;
 
     private static final Log LOG = LogFactory.getLog(MyIntegrator.class);
 
     @Override
     public void integrate(Metadata metadata, SessionFactoryImplementor sessionFactory, SessionFactoryServiceRegistry serviceRegistry) {
 
-        if (saveOrUpdateEventListeners != null && !saveOrUpdateEventListeners.isEmpty()) {
-            serviceRegistry.getService(EventListenerRegistry.class)
-                    .appendListeners(EventType.SAVE_UPDATE, toArray(saveOrUpdateEventListeners));
+        Field[] declaredFields = EventType.class.getDeclaredFields();
+
+        for (Field declaredField : declaredFields) {
+            if(
+                    Modifier.isFinal(declaredField.getModifiers()) &&
+                            Modifier.isPublic(declaredField.getModifiers()) &&
+                            Modifier.isStatic(declaredField.getModifiers()) &&
+                            declaredField.getType().isAssignableFrom(EventType.class)
+                    ){
+                try {
+                    EventType eventType = (EventType) declaredField.get(null);
+                    Class listenerInterface = eventType.baseListenerInterface();
+                    Object[] objects = toArray(beanFactory.getBeansOfType(listenerInterface).values());
+                    if(objects.length > 0){
+                                    serviceRegistry.getService(EventListenerRegistry.class).appendListeners(eventType, objects);
+                    }
+
+                } catch (IllegalAccessException e) {
+                    //should never happen
+                    throw new RuntimeException(e);
+                }
+
+            }
         }
 
         LOG.debug("integrate");
@@ -40,7 +62,7 @@ public class MyIntegrator implements Integrator {
 
     @SuppressWarnings("unchecked")
     private <T> T[] toArray(Collection<T> source) {
-        return (T[])source.toArray();
+        return (T[]) source.toArray();
     }
 
     @Override
